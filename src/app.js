cadf4b90d1aabcb7fbe0735f0486844010b47255
ratnadeepdey1998 <ratnadeepdey13@gmail.com>
2022-06-14 12:12:53 +0530

    change to port number 3020

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ require('dotenv').config()
 const express=require("express");
 const app=express();
 const path=require("path");
-const port=process.env.PORT || 3000;
+const port=process.env.PORT || 3020;
 const stateP=path.join(__dirname, "../public");
 
 app.use(express.static(stateP));
